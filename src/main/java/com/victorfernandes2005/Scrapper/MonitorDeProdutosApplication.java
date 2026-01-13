f71be1ee62dd9af2362db8dfd4f0754650731354
VictorFernandes2005 <fernandes.victor2708@gmail.com>
2026-01-13 08:29:35 -0300

    First Commit!

---
+++ src/main/java/com/victorfernandes2005/Scrapper/MonitorDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.victorfernandes2005.Scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorfernandes2005/Scrapper/MonitorDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorfernandes2005.Scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
