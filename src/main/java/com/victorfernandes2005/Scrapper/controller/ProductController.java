59482fa9e1ac0be07936f2c8b981d12d725b9681
VictorFernandes2005 <fernandes.victor2708@gmail.com>
2026-01-20 09:11:16 -0300

    Feat: Create ProductController

---
+++ src/main/java/com/victorfernandes2005/Scrapper/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.victorfernandes2005.Scrapper.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import com.victorfernandes2005.Scrapper.model.ProductModel;
+import com.victorfernandes2005.Scrapper.service.ProductService;
+
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    ProductService service;
+
+    @GetMapping("products/")
+    public String viewProductsGet(Model model){
+        return "products/get_products";
+    }
+
+    @GetMapping("products/add")
+    public String addPageGet(Model model){
+        ProductModel product = new ProductModel();
+        model.addAttribute("product",product);
+        return "products/add_new_product";
+    }
+
+    @PostMapping("products/add")
+    public String addPagePost(@RequestBody ProductModel product, BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("product",product);
+            model.addAttribute("error",result.getAllErrors());
+            return "products/add_new_product";
+        }
+        return "products/get_products";
+    }
+}
